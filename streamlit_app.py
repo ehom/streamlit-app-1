60ca61fc219d035d252214a65619626af1b89d90
ehom <ehom@acm.org>
2023-06-16 21:23:19 -0700

    working version

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.header('Introduction to TensorFlow in Artifical Intelligence, Machine Learning, and Deep Learning')
+
+st.write('Assignment 1')
+
+
+if st.button('Create Model'):
+    st.write('Creating Model..')
